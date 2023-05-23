f6f5e2cda3be0ac95af0c9ddd9f173c1e6c41243
Kevin Koshy <kevin_koshy@hotmail.com>
2023-05-23 17:25:45 +0530

    Final commit

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 #include <vector>
 #include "Account.h"
 #include "Savings_Account.h"
-//#include "Checking_Account.h"
-//#include "Trust_Account.h"
+#include "Checking_Account.h"
+#include "Trust_Account.h"
 #include "Account_Util.h"
 
 using namespace std; 
@@ -15,9 +15,21 @@ int main() {
     cout << fixed;
    
 
-Account *ptr = new Trust_Account("Leo", 10000, 2.6);
-cout << *ptr <<endl;
-    
+    Account *trust = new Trust_Account("James");
+    cout <<*trust<<endl;
+
+    Account *p1 = new Checking_Account("Larry", 100);
+    Account *p2 = new Savings_Account("Moe", 500,3);
+    Account *p3 = new Trust_Account("Curly", 900, 0.5);
+
+    std::vector<Account *> accounts{p1, p2, p3};
+
+    display(accounts);
+    deposit(accounts, 2300);
+    withdraw(accounts, 1000);
+
+    display(accounts);
+
     return 0;
 }
 
